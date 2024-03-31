5e9388a14a9af6e54a0a45f5307b239a2a13c882
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-04-01 01:18:29 +0530

    updated the folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -5,18 +5,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
 import About from "./Components/About.jsx";
-import Cart from "./Components/Cart.jsx";
+import Cart from "./Components/NavBar/Cart.jsx";
 import FoodTypeRes from "./Components/FoodTypeRes.jsx";
-import Help from "./Components/Help.jsx";
-import Login from "./Components/Login.jsx";
+import Help from "./Components/NavBar/Help.jsx";
+import Login from "./Components/NavBar/Login.jsx";
 import Resturants from "./Components/Resturants.jsx";
 import ResturantMenu from "./Components/ResturantMenu.jsx";
-import Signup from "./Components/Signup.jsx";
-import SetLocation from "./Components/SetLocation.jsx";
-import Search from "./Components/Search.jsx";
+import Signup from "./Components/NavBar/Signup.jsx";
+import SetLocation from "./Components/NavBar/SetLocation.jsx";
+import Search from "./Components/NavBar/Search.jsx";
 
 import "./index.css";
-import Offers from "./Components/Offers.jsx";
+import Offers from "./Components/NavBar/Offers.jsx";
 
 const appRoutes = createBrowserRouter([
   {
