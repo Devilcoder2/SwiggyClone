d278c0dbd564324dae9625bedfaf22dba935da89
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-04-03 02:06:07 +0530

    started with the offers page

--- src/Components/NavBar/Offers.jsx
+++ src/Components/NavBar/Offers.jsx
@@ -1,5 +1,21 @@
 const Offers = () => {
-  return <h1>This is the offers card</h1>;
+  return (
+    <>
+      <div className="ml-[200px] mt-16">
+        <div className="mb-4">
+          <h1 className="text-md ">
+            Home/<span className="text-gray-600">Offers</span>
+          </h1>
+        </div>
+
+        <div>
+          <h1 className="text-gray-900 text-2xl font-bold ">
+            Restaurants With Great Offers Near Me
+          </h1>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Offers;
