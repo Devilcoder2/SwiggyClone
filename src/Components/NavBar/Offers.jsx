f3b1a61a1b7a6fc23a58334c9129ae5824a1bca2
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-04-09 01:02:56 +0530

    started with the filter section

--- src/Components/NavBar/Offers.jsx
+++ src/Components/NavBar/Offers.jsx
@@ -9,8 +9,14 @@ import ShimmerCard from "../Shimmer/ShimmerCard";
 
 const Offers = () => {
   const [offersResList, setOffersResList] = useState([]);
+  const [filteredOffersResList, setFilteredOffersResList] = useState([]);
+  const [color, setColor] = useState(true);
 
   const cardArray = new Array(9).fill(null);
+
+  useEffect(() => {
+    fetchapi();
+  }, []);
 
   const fetchapi = async () => {
     const res = await fetch(
@@ -23,11 +29,46 @@ const Offers = () => {
       data.data.cards[4].card.card.gridElements.infoWithStyle.restaurants;
 
     setOffersResList(restaurantData);
+    setFilteredOffersResList(restaurantData);
   };
 
-  useEffect(() => {
-    fetchapi();
-  }, []);
+  const clickHandler = (i) => {
+    if (i == 1) {
+      console.log("clicked on first");
+    } else if (i == 2) {
+      console.log("clicked on second");
+    } else if (i == 3) {
+      const newList = offersResList.filter(
+        (t) => t.info.sla.deliveryTime <= 30
+      );
+      setFilteredOffersResList(newList);
+      setColor(!color);
+    } else if (i == 5) {
+      const newList = offersResList.filter((t) => t.info.avgRating > 4);
+      setFilteredOffersResList(newList);
+    } else if (i == 6) {
+      const newList = offersResList.filter((t) => t.info.veg === true);
+      setFilteredOffersResList(newList);
+    } else if (i == 7) {
+      setFilteredOffersResList(offersResList);
+    } else if (i == 8) {
+      const newList = offersResList.filter((t) => {
+        const str = t.info.costForTwo;
+        const match = str.match(/\d+/);
+        const value = parseInt(match[0]);
+        return value <= 600;
+      });
+      setFilteredOffersResList(newList);
+    } else if (i == 9) {
+      const newList = offersResList.filter((t) => {
+        const str = t.info.costForTwo;
+        const match = str.match(/\d+/);
+        const value = parseInt(match[0]);
+        return value <= 300;
+      });
+      setFilteredOffersResList(newList);
+    }
+  };
 
   return (
     <>
@@ -47,36 +88,88 @@ const Offers = () => {
         <div className="mb-4">
           <ul className="flex mt-3">
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Filter</button>
+              <button
+                onClick={() => {
+                  clickHandler(1);
+                }}
+              >
+                Filter
+              </button>
             </li>
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Sort By</button>
+              <button
+                onClick={() => {
+                  clickHandler(2);
+                }}
+              >
+                Sort By
+              </button>
+            </li>
+            <li
+              className={
+                color === true
+                  ? "border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2"
+                  : "border border-gray-700 bg-gray-200 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2"
+              }
+            >
+              <button
+                onClick={() => {
+                  clickHandler(3);
+                }}
+              >
+                Fast Delivery
+              </button>
+            </li>
+
+            <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
+              <button
+                onClick={() => {
+                  clickHandler(5);
+                }}
+              >
+                Ratings 4.0+
+              </button>
             </li>
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Fast Delivery</button>
+              <button
+                onClick={() => {
+                  clickHandler(6);
+                }}
+              >
+                Pure Veg
+              </button>
             </li>
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>New on Swiggy</button>
+              <button
+                onClick={() => {
+                  clickHandler(7);
+                }}
+              >
+                Offers
+              </button>
             </li>
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Ratings 4.0+</button>
+              <button
+                onClick={() => {
+                  clickHandler(8);
+                }}
+              >
+                Rs.300-Rs.600
+              </button>
             </li>
             <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Pure Veg</button>
-            </li>
-            <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Offers</button>
-            </li>
-            <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Rs.300-Rs.600</button>
-            </li>
-            <li className="border border-gray-300 text-gray-800 text-sm rounded-full mr-4 px-2 pt-1 pb-2">
-              <button>Less than Rs.300</button>
+              <button
+                onClick={() => {
+                  clickHandler(9);
+                }}
+              >
+                Less than Rs.300
+              </button>
             </li>
           </ul>
         </div>
 
-        {offersResList.length === 0 && (
+        {filteredOffersResList.length === 0 && (
           <div className="flex flex-wrap justify-center ml-[-90px]">
             {cardArray.map((_, index) => (
               <ShimmerCard key={index} />
@@ -85,7 +178,7 @@ const Offers = () => {
         )}
 
         <div className="flex flex-wrap justify-center ml-[-90px]">
-          {offersResList.map((t) => {
+          {filteredOffersResList.map((t) => {
             const imageHeader = t.info.aggregatedDiscountInfoV3?.header;
 
             if (imageHeader) {
