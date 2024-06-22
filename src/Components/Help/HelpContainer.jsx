7681d1b59efefd822434f08d4c21f4a0922e49b9
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-06-22 20:38:45 +0530

    Implemented the layout of the help component

---
+++ src/Components/Help/HelpContainer.jsx
@@ -0,0 +1,5 @@
+const HelpContainer = () => {
+  return <div className="w-[80rem] h-[45rem] bg-white"></div>;
+};
+
+export default HelpContainer;
--- src/Components/NavBar/Help.jsx
+++ src/Components/NavBar/Help.jsx
@@ -1,5 +1,18 @@
+import HelpContainer from "../Help/HelpContainer";
+
 const Help = () => {
-  return <div>This is help page</div>;
+  return (
+    <div className="bg-[#37718E] h-[120vh] -mt-4 text-white">
+      <div className="pt-12 pl-40">
+        <h1 className="text-3xl font-bold">Help & Support</h1>
+        <p>Let's take a step ahead and help you better.</p>
+      </div>
+
+      <div className="pl-32 mt-8">
+        <HelpContainer />
+      </div>
+    </div>
+  );
 };
 
 export default Help;
